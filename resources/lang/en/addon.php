7774384c7e2cb6658a454bec5a545eedae7b62b0
Ryan Thompson <ryan@pyrocms.com>
2015-09-21 15:06:47 -0500

    Initial commit

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Forms',
+    'name'        => 'Forms Module',
+    'description' => ''
+];
---
+++ src/FormsModule.php
@@ -0,0 +1,24 @@
+<?php namespace Anomaly\FormsModule;
+
+use Anomaly\Streams\Platform\Addon\Module\Module;
+
+class FormsModule extends Module
+{
+
+    /**
+     * The navigation icon.
+     *
+     * @var string
+     */
+    protected $icon = 'addon';
+
+    /**
+     * The module sections.
+     *
+     * @var array
+     */
+    protected $sections = [
+        'example'
+    ];
+
+}
