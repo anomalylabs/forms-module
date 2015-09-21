da94fe915f1c002f00f5eee99dccbc55e5946b1f
Ryan Thompson <ryan@pyrocms.com>
2015-09-21 15:23:03 -0500

    Yesh

--- migrations/2015_09_21_200616_anomaly.module.forms__create_forms_fields.php
+++ migrations/2015_09_21_200616_anomaly.module.forms__create_forms_fields.php
@@ -2,6 +2,13 @@
 
 use Anomaly\Streams\Platform\Database\Migration\Migration;
 
+/**
+ * Class AnomalyModuleFormsCreateFormsFields
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ */
 class AnomalyModuleFormsCreateFormsFields extends Migration
 {
 
@@ -10,6 +17,14 @@ class AnomalyModuleFormsCreateFormsFields extends Migration
      *
      * @var array
      */
-    protected $fields = [];
+    protected $fields = [
+        'form_name' => 'anomaly.field_type.text',
+        'form_slug' => [
+            'type'   => 'anomaly.field_type.slug',
+            'config' => [
+                'slugify' => 'form_name'
+            ]
+        ]
+    ];
 
 }
