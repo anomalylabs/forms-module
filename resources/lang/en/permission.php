fcc0e147aab344ededdcf59b7c84af6791f8c471
Ryan Thompson <ryan@pyrocms.com>
2016-01-18 12:58:09 -0600

    Create permission.php

---
+++ resources/lang/en/permission.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'forms'         => [
+        'name'   => 'Forms',
+        'option' => [
+            'read'   => 'Can access forms section.',
+            'write'  => 'Can create and edit forms.',
+            'delete' => 'Can delete forms.'
+        ]
+    ],
+    'notifications' => [
+        'name'   => 'Notifications',
+        'option' => [
+            'read'   => 'Can access notifications section.',
+            'write'  => 'Can create and edit notifications.',
+            'delete' => 'Can delete notifications.'
+        ]
+    ],
+    'fields'        => [
+        'name'   => 'Fields',
+        'option' => [
+            'manage' => 'Can manage custom fields.'
+        ]
+    ]
+];
