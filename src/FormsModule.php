02b12e6e5341bb1c721c852822e6968164fa99dd
Ryan Thompson <ryan@pyrocms.com>
2017-02-05 19:28:32 -0600

    Fix entries breadcrumb

--- src/FormsModule.php
+++ src/FormsModule.php
@@ -14,6 +14,51 @@ class FormsModule extends Module
 {
 
     /**
+     * The module sections.
+     *
+     * @var array
+     */
+    protected $sections = [
+        'forms'         => [
+            'buttons' => [
+                'new_form' => [
+                    'data-toggle' => 'modal',
+                    'data-target' => '#modal',
+                    'href'        => 'admin/forms/choose',
+                ],
+            ],
+        ],
+        'entries'       => [
+            'parent' => 'forms',
+            'href'   => 'admin/forms/entries/{request.route.parameters.form}',
+        ],
+        'assignments'   => [
+            'parent'  => 'forms',
+            'buttons' => [
+                'add_field' => [
+                    'data-toggle' => 'modal',
+                    'data-target' => '#modal',
+                    'href'        => 'admin/forms/assignments/{request.route.parameters.id}/choose',
+                ],
+            ],
+        ],
+        'notifications' => [
+            'buttons' => [
+                'new_notification',
+            ],
+        ],
+        'fields'        => [
+            'buttons' => [
+                'new_field' => [
+                    'data-toggle' => 'modal',
+                    'data-target' => '#modal',
+                    'href'        => 'admin/forms/fields/choose',
+                ],
+            ],
+        ],
+    ];
+
+    /*
      * The addon icon.
      *
      * @var string
