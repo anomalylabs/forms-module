eb5134f31437c66b888ab4accbc662a5cd0559c9
Ryan Thompson <ryan@pyrocms.com>
2017-10-13 11:49:02 -0500

    Attachments should support other types.

--- src/Form/FormMailer.php
+++ src/Form/FormMailer.php
@@ -3,22 +3,21 @@
 use Anomaly\FilesModule\File\Contract\FileInterface;
 use Anomaly\FormsModule\Form\Contract\FormInterface;
 use Anomaly\FormsModule\Form\Contract\FormRepositoryInterface;
-use Anomaly\FormsModule\Notification\Contract\NotificationInterface;
 use Anomaly\Streams\Platform\Assignment\Contract\AssignmentInterface;
 use Anomaly\Streams\Platform\Entry\Contract\EntryInterface;
 use Anomaly\Streams\Platform\Support\Value;
 use Anomaly\Streams\Platform\Ui\Form\FormBuilder;
 use Anomaly\WysiwygFieldType\WysiwygFieldType;
+use Illuminate\Contracts\Config\Repository;
 use Illuminate\Contracts\Mail\Mailer;
 use Illuminate\Mail\Message;
 
 /**
  * Class FormMailer
  *
- * @link          http://anomaly.is/streams-platform
- * @author        AnomalyLabs, Inc. <hello@anomaly.is>
- * @author        Ryan Thompson <ryan@anomaly.is>
- * @package       Anomaly\FormsModule\Form
+ * @link   http://pyrocms.com/
+ * @author PyroCMS, Inc. <support@pyrocms.com>
+ * @author Ryan Thompson <ryan@pyrocms.com>
  */
 class FormMailer
 {
@@ -45,26 +44,41 @@ class FormMailer
     protected $mailer;
 
     /**
+     * The config repository.
+     *
+     * @var Repository
+     */
+    protected $config;
+
+    /**
      * Create a new FormMailer instance.
      *
-     * @param Mailer $mailer
-     * @param Value  $value
+     * @param Mailer                  $mailer
+     * @param Repository              $config
+     * @param Value                   $value
+     * @param FormRepositoryInterface $forms
      */
-    public function __construct(Mailer $mailer, Value $value)
+    public function __construct(Mailer $mailer, Repository $config, Value $value, FormRepositoryInterface $forms)
     {
         $this->mailer = $mailer;
+        $this->config = $config;
         $this->value  = $value;
+        $this->forms  = $forms;
     }
 
     /**
      * Send the form message.
      *
-     * @param FormInterface $form
-     * @param FormBuilder   $builder
+     * @param FormInterface           $form
+     * @param FormRepositoryInterface $forms
+     * @param FormBuilder             $builder
      */
     public function send(FormInterface $form, FormBuilder $builder)
     {
-        $input = $builder->getFormEntry();
+        $input = $entry = $builder->getFormEntry();
+
+        $stream = $entry->getStream();
+        $form   = $this->forms->findBySlug($stream->getSlug());
 
         if (!$form->shouldSendNotification()) {
             return;
@@ -74,35 +88,31 @@ class FormMailer
             return;
         }
 
-        /* @var NotificationInterface $notification */
         $notification = $form->getNotification();
 
         /* @var WysiwygFieldType $email */
-        $email  = $notification->getFieldType('notification_content');
-        $layout = $notification->getFieldValue('notification_email_layout');
-
-        $content = $email->getViewPath();
+        $email = $notification->getFieldType('notification_content');
 
         $this->mailer->send(
-            $layout ?: 'anomaly.module.forms::email',
-            compact('input', 'form', 'content'),
-            function (Message $message) use ($form, $input, $builder, $notification) {
+            $email->getViewPath(),
+            compact('input', 'form'),
+            function (Message $message) use ($form, $entry, $builder, $notification) {
 
                 $message->cc($form->getNotificationCc());
                 $message->bcc($form->getNotificationBcc());
                 $message->to($form->getNotificationSendTo());
-                $message->subject($this->value->make($notification->getNotificationSubject(), $input, 'input'));
-                $message->sender($this->value->make($notification->getNotificationFromEmail(), $input, 'input'));
+                $message->subject($this->value->make($notification->getNotificationSubject(), $entry, 'input'));
+                $message->sender($this->value->make($notification->getNotificationFromEmail(), $entry, 'input'));
                 $message->replyTo(
-                    $this->value->make($notification->getNotificationReplyToEmail(), $input, 'input'),
-                    $this->value->make($notification->getNotificationReplyToName(), $input, 'input')
+                    $this->value->make($notification->getNotificationReplyToEmail(), $entry, 'input'),
+                    $this->value->make($notification->getNotificationReplyToName(), $entry, 'input')
                 );
                 $message->from(
-                    $this->value->make($notification->getNotificationFromEmail(), $input, 'input'),
-                    $this->value->make($notification->getNotificationFromName(), $input, 'input')
+                    $this->value->make($notification->getNotificationFromEmail(), $entry, 'input'),
+                    $this->value->make($notification->getNotificationFromName(), $entry, 'input')
                 );
 
-                $this->attachFiles($message, $input);
+                $this->attachFiles($message, $entry);
             }
         );
     }
@@ -115,11 +125,17 @@ class FormMailer
      */
     protected function attachFiles(Message $message, EntryInterface $entry)
     {
-        /* @var AssignmentInterface $assignment */
-        foreach ($entry->getAssignmentsByFieldType('anomaly.field_type.file') as $assignment) {
-            /* @var FileInterface $file */
-            if ($file = $entry->{$assignment->getFieldSlug()}) {
-                $message->attachData($file->resource()->read(), $file->getName());
+        $supported = $this->config->get('anomaly.module.forms::attachments.supported', []);
+
+        foreach ($supported as $type) {
+
+            /* @var AssignmentInterface $assignment */
+            foreach ($entry->getAssignmentsByFieldType($type) as $assignment) {
+
+                /* @var FileInterface $file */
+                if ($file = $entry->{$assignment->getFieldSlug()}) {
+                    $message->attachData($file->resource()->read(), $file->getName());
+                }
             }
         }
     }
